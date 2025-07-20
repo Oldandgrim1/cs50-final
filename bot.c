23aa2472edb631f8c03ef86591514e803cf3a27b
Oldandgrim1 <joshua.meador@gmail.com>
2025-07-20 13:18:18 -0700

    Got test code working, compiled WiringPi.h and added it

--- bot.c
+++ bot.c
@@ -1,1 +1,20 @@
-// Test comment.
+// Initialize headers. 
+#include <stdio.h>
+#include <wiringPi.h>
+#define LED_PIN 18 // gpio18 - physical pin 12, see pinout.xyz for info.
+// Test code to light LEDs in sequence.
+int main (void)
+{
+    if (wiringPiSetupGpio() == -1) 
+    {
+        printf("Failed to initialize WiringPi\n");
+        return 1;
+    }
+    pinMode(LED_PIN, OUTPUT);
+    // Turn pin on.
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    // Turn pin off.
+    digitalWrite(LED_PIN, LOW);
+}
+
